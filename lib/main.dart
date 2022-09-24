e99dd74eec81a019cf4fe0951796769be50c0816
Tomoya <187tomo@gmail.com>
2022-09-24 04:23:16 -0400

    Final commit of vid #18
    https://www.youtube.com/watch?v=XIxahpXU_QE&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ&index=19

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import "quote.dart";
 void main() => runApp(MaterialApp(
   home: QuoteList(),
 
@@ -14,10 +14,10 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+  List<Quote> quotes = [
+    Quote(author: "Oscar Wilde" , text: "This is the majestic text"),
+    Quote(author: "Trudy Wilthman" , text: "This is the majestic text"),
+    Quote(author: "Henry Ford" , text: "This is the majestic text")
   ];
 
 
@@ -33,7 +33,7 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes.map((quote) {
-          return Text(quote);
+          return Text("${quote.text} - ${quote.author}");
         }).toList(),
       )
     );
--- lib/quote.dart
+++ lib/quote.dart
@@ -1,7 +1,14 @@
 class Quote {
 
+
   String? text;
   String? author;
+
+  Quote({String? text, String? author}) {
+  this.text = text!;
+  this.author = author!;
+  }
+
 
 
 }
